c1934cab1e24990b4a0f17ca43adf98057e7ee22
sushantdhruv2003 <121683440+sushantdhruv2003@users.noreply.github.com>
2024-12-17 20:13:31 +0000

    Truthy and Falsy Statement

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,9 @@
+// truthy and falsy value:
+
+// falsy values:
+// false, 0, -0, BigInt(0n), "", null, undefined, NaN
+
+// truthy values:
+// "0", "false", " ", [] empty array, {} empty object, function(){}
+// jo v string hai wo to pakka hoga truthy value
+
