afa49712d5ee8480cc81522d50ac924fa0c447a4
sushantdhruv2003 <121683440+sushantdhruv2003@users.noreply.github.com>
2024-12-17 20:12:50 +0000

    if-else

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,45 @@
+// = : assignment keliye
+// == : barabar
+// === : strict checking (data types v check karta hai), value and data type dono same hona chahiye)
+
+// const temp = 44
+// if(temp < 50){
+//     console.log("Temperature is less than 50 degree")
+// }
+// else{
+//     console.log("Temperature is greater than 50 degree")
+// }
+
+// const balance = 1000
+// if(balance > 500) console.log("Test")
+
+// const balance = 1000
+// if(balance < 500){
+//     console.log("Less than 500")
+// }
+// else if(balance < 750){
+//     console.log("Less than 750");
+// }
+// else if(balance < 900){
+//     console.log("Less than 900");
+// }
+// else{
+//     console.log("Less than 1200");
+// }
+
+// const isLoggedIn = true
+// const isAvailable = true
+// if(isLoggedIn && isAvailable){
+//     console.log("Valid");
+// }
+// else{
+//     console.log("Invalid");
+// }
+
+// Terniary Operator:
+
+// Syntax:
+// condition ? true : false
+
+const price = 100
+price < 200 ? console.log("Buy It") : console.log("Don't Buy")
