abfe66be57382dadcc7a876a8a868cff4c08eb1b
sushantdhruv2003 <121683440+sushantdhruv2003@users.noreply.github.com>
2024-12-18 07:44:09 +0000

    For Loop

---
+++ 05_iterations/01_forLoop.js
@@ -0,0 +1,36 @@
+// loop inside loop:
+// for(let i = 0; i <= 2; i++){
+//     console.log(`Outer Loop value: ${i}`)
+//     for(let j = 0; j <= 2; j++){
+//         console.log(i + '*' + j + ' = ' + i*j)
+//     }
+// }
+
+// let myAlphabets = ["A", "B", "C", "D"]
+// for(let index = 0; index < myAlphabets.length; index++){
+//     const elements = myAlphabets[index]
+//     console.log(elements)
+// }
+
+// Keywords:
+
+// break:
+
+// for(let index = 1; index <= 10; index++){
+//     if(index == 5){
+//         console.log("5 is detected")
+//         break // 4 tak hi bs print hoga uske baad nahi
+//     }
+//     console.log(`Value is: ${index}`)
+// }
+
+// continue: ek ko bs hata denge
+
+// for(let index = 1; index <= 10; index++){
+//     if(index == 5){
+//         console.log("5 is detected")
+//         continue // bs 5 print nahi hoga, baki sara print hoga 5 se aage piche wala sb
+//     }
+//     console.log(`Value is: ${index}`)
+// }
+
