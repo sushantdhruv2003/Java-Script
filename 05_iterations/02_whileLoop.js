7d39e05901b1e292d20d1ff94026e1faadce8c56
sushantdhruv2003 <121683440+sushantdhruv2003@users.noreply.github.com>
2024-12-18 07:44:32 +0000

    While And Do-While Loop

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,25 @@
+// let index = 0
+// while(index <= 10){
+//     console.log(`Value is: ${index}`)
+//     index += 2
+// }
+
+// do-while loop: (Kaam phle condition baad me check hota hai)
+
+// let score = 1
+// do{
+//     console.log(`Score is ${score}`)
+//     score++
+// }
+// while(score <= 10)
+
+// if value is out of condition:
+
+// let score = 11
+// do{
+//     console.log(`Score is ${score}`)
+//     score++
+// }
+// while(score <= 10)
+
+// Output: Score is 11
