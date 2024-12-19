e17331d16e6bfe7e7f26bad596906d9b66813345
sushantdhruv2003 <121683440+sushantdhruv2003@users.noreply.github.com>
2024-12-19 19:40:05 +0000

    for-of and for-in loop

---
+++ 05_iterations/03_otherLoops.js
@@ -0,0 +1,42 @@
+// for of loop:
+// const greeting = "Hello World"
+// for(const i of greeting){
+//     if(i == " "){
+//         continue // space print nahi hoga
+//     }
+//     console.log(`Each char is ${i}`) 
+// }
+
+// Maps:
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States Of America")
+map.set('Fr', "France")
+map.set('IN', "India") // duplicate print nahi hoga
+
+// console.log(map)
+
+// using for of loop:
+// for(const [key, value] of map){
+//     console.log(key, ':', value)
+// }
+
+// accessing for objects:
+// (FOR IN LOOP):
+// const myObj = {
+//     js: "JavaScript",
+//     cpp: "C++",
+//     py: "Python",
+//     rb: "Ruby",
+//     swift: "Swift By Apple"
+// }
+// for(const i in myObj){
+//     console.log(`${i} is the shortcut for ${myObj[i]}`)
+// }
+
+// using for in loop in arrays:
+// const arr = ['js', 'py', 'cpp', 'rb']
+// for(const i in arr){
+//     console.log(arr[i])
+// }
+
